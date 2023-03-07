f27497f29e5b57dcb7f57a680c23b87af4b8613e
Zac Waite <129032699zw@gmail.com>
2023-03-06 22:29:21 -0500

    Initial nuxt website

---
+++ components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+	<main>
+		<h1>Nitron</h1>
+	</main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Home/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  head() {
+    return {
+      title: 'Nitron',
+      meta: [
+        { name: 'description', content: 'Unlocking the Authenticity of Emotion to bring Virtual Worlds to life' },
+      ],
+    }
+  },
+})
+</script>
