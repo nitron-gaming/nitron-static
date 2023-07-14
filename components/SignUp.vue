5d655bc230bb6dc4e0fa987fc70b1e14890e1eea
Zac Waite <129032699zw@gmail.com>
2023-07-13 23:49:15 -0400

    Sign Up with Google

--- components/SignUp.vue
+++ components/SignUp.vue
@@ -6,8 +6,12 @@
         </div>
 		<div v-else-if="!isAuthenticated" class="sign-up-buttons">
 			<button class="github-button" @click="signUpWithGithub">
-				<p>Sign Up with Github</p>
 				<img src="/github-mark-white.svg" alt="Github logo" />
+				<p>Sign Up with Github</p>
+			</button>
+			<button class="google-button" @click="signUpWithGoogle">
+				<img src="/google-icon.svg" alt="Google logo" />
+				<p>Sign Up with Google</p>
 			</button>
 		</div>
 		<div v-else>
@@ -27,30 +31,53 @@
 	}
 	.sign-up-buttons {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
+		align-items: center;
 		margin-top: 2rem;
+		button {
+			margin: 1rem 0;
+			max-width: 20rem;
+			border: none;
+			// height: 3rem;
+			border-radius: 5px;
+			cursor: pointer;
+			display: flex;
+			align-items: center;
+			img {
+				margin-right: 1rem;
+			}
+			p {
+				color: white;
+				font-size: 1.5rem;
+			}
+		}
 	}
 	.github-button {
 		background-color: #24292e;
-		color: white;
-		border: none;
-		padding: 10px 20px;
-		border-radius: 5px;
-		font-size: 16px;
-		cursor: pointer;
-		display: flex;
-		align-items: center;
+		padding: 0.6rem 1rem;
 		&:hover {
 			background-color:rgb(74, 82, 90);
-		}
-		p {
-			color: white;
-			font-size: 1.5rem;
 		}
 		img {
 			width: 2rem;
 			height: 2rem;
-			margin-left: 1rem;
+		}
+	}
+	.google-button {
+		background-color: #4285F4;
+		padding: 0.2rem;
+		&:hover {
+			background-color:#2d69cb;
+		}
+		img {
+			background-color: white;
+			padding: 0.5rem;
+			height: 1.5rem;
+			width: 1.5rem;
+		}
+		p {
+			margin-right: 1rem;
 		}
 	}
 	p {
@@ -63,7 +90,7 @@
 
 <script lang="ts">
 import { auth, db } from '~/plugins/firebase'
-import { GithubAuthProvider, signInWithPopup } from 'firebase/auth'
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { doc, setDoc } from 'firebase/firestore'
 import { sendEmailVerification } from "firebase/auth";
 import { mapGetters } from 'vuex';
@@ -94,7 +121,28 @@ export default {
             } catch (error) {
                 console.error("Error signing up with Github: ", error);
             }
-        }
-    }
+        },
+		async signUpWithGoogle() {
+		  const provider = new GoogleAuthProvider()
+		  try {
+			const result = await signInWithPopup(auth, provider)
+			const user = result.user as any
+			await setDoc(doc(db, 'users', user.uid), {
+			  displayName: user.displayName,
+			  email: user.email,
+			  photoURL: user.photoURL,
+			  googleAccessToken: user.accessToken,
+			})
+			if (auth.currentUser) await sendEmailVerification(auth.currentUser)
+			// Update the Vuex store
+			// this.$store.dispatch('', {
+			//   displayName: user.displayName,
+			//   email: user.email
+			// });
+		  } catch (error) {
+			console.error('Error signing up with Google:', error)
+		  }
+		},
+    },
 }
 </script>
